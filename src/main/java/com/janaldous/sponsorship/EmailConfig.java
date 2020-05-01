e589c5276279b26eaa9247bf97c3bbc5941996d6
janaldoustorres <jat.torres@gmail.com>
2020-05-02 02:35:16 +0800

    bug fix

--- src/main/java/com/janaldous/sponsorship/EmailConfig.java
+++ src/main/java/com/janaldous/sponsorship/EmailConfig.java
@@ -15,9 +15,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.janaldous.sponsorship.application.domain.EmailProperties;
 
+@PropertySource("classpath:email.properties")
 @Configuration
 public class EmailConfig {
 
